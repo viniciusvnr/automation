7583a96ccf05b289c5fe7e81fd7fc3c685e643a6
Vinícius Nunes Rocha <vrocha@braspag.com.br>
2019-07-22 14:59:11 -0300

    start mvp

--- qualys/cicontainersecurity.py
+++ qualys/cicontainersecurity.py
@@ -1,1 +1,19 @@
-import requests
+import qualysapi
+
+class QualysServerCreds:
+
+    def __init__(self, serverUrl, username, password):
+
+        serverUrl = serverUrl.replace("/$", "")
+        self.serverUrl = serverUrl
+        self.username = username
+        self.password = password
+
+
+class GetImageVul:
+    pass
+
+class GetBuildInfo:
+    pass
+
+
