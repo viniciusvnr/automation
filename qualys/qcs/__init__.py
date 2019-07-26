87e38f5be38000ccb6030f6cf347323877ff9e6e
Vinícius Nunes Rocha <vrocha@braspag.com.br>
2019-07-26 12:40:27 -0300

    add Ownership and Copyright Information

---
+++ qualys/qcs/__init__.py
@@ -0,0 +1,6 @@
+# Ownership and Copyright Information.
+from __future__ import absolute_import
+
+__author__ = "Vinicius Rocha <viniciusvnr@gmail.com>"
+__copyright__ = "Copyright 2018-2019, Vinicius Rocha"
+__license__ = "NA"
